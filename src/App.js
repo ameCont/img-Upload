0ff2dc6c206dd7214cddbfcde4f3e3606c0ca705
ameCont <americancont7@gmail.com>
2020-05-26 09:44:19 +0300

    imgupload V0

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+/*
+var express = require('express');
+var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+
+class App extends Component {
+  state = {
+    selectedFile: null
+  }
+
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile : event.target.files[0]
+    })
+
+  }
+
+  fileUploadHandler = () => {
+     const fd = new FormData();
+     fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
+     axios.post('https://us-central1-upload-img-f9afa.cloudfunctions.net/uploadFile', fd)
+     .then(res => {
+       console.log(res);
+     });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="file" onChange={this.fileSelectedHandler}/>
+        <button onClick={this.fileUploadHandler}>Upload</button>
+      </div>
+    );
+  }
+}
+
+export default App;
