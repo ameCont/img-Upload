a7c4d12c9272e6df28a0de700a4c2e8220965aa6
ameCont <americancont7@gmail.com>
2020-05-26 09:51:54 +0300

    imgupload V0

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
 
-/*
+
 var express = require('express');
 var app = express();
 app.use(function(req, res, next) {
@@ -11,7 +11,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-*/
+
 
 class App extends Component {
   state = {
